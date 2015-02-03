cd3113e92354b0d863b52e9f770f93a5c2b13b05
Megan Karbowski <mkarbows@lion.lmu.edu>
2015-02-03 15:52:41 -0800

    updated working ChangeMaker.java
    working version

--- moneyAndStrings/ChangeMaker.java
+++ moneyAndStrings/ChangeMaker.java
@@ -1,36 +1,45 @@
 public class ChangeMaker {
-	public static int getQuarters(int cents){
+
+	public static int getQuarters(int cents) {
 		return cents / 25; 
 		}
-	public static int getDimes(int cents){
+	public static int getDimes(int cents) {
 		return cents / 10;
 	}
-	public static int getNickels(int cents){
+	public static int getNickels(int cents) {
 		return cents / 5;
 	}
-	public static int getPennies(int cents){
+	public static int getPennies(int cents) {
 		return cents / 1;
 	}
-	public static int[] getChange(int cents){
+	public static int[] getChange(int cents) {
 		int[] changeArray = new int[4];
 		changeArray[0] = getQuarters(cents);
 		changeArray[1] = getDimes(cents);
 		changeArray[2] = getNickels(cents);
 		changeArray[3] = getPennies(cents);
-		return changeArray;
+				return changeArray;
+
 	}
-	public static void main(String[] args) {
-		try{
+
+	public static void main(String args[]) {
+		try {
 			int number = Integer.parseInt(args[0]);
-		if(number < 0){
-			System.out.println("please enter a positive number");
-		} else{
+			if (number < 0) {
+				System.out.println("please enter a positive number");
+			} 
+			else {
+				int[] finalArray = getChange(number);
+				for (int i = 0; i < 4; i++) {
+					System.out.println(finalArray[i]);
+				}
 
+			} 
 		}
-
-		}catch(Exception e) {
+		catch(Exception e) {
 			System.out.println("please enter an integer");
 		}
 
+		
 	}
 }
