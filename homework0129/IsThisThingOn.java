ddc761a1087f24895fd524ebb1ebbdb5c5eb6032
Megan Karbowski <mkarbows@lion.lmu.edu>
2015-02-04 14:12:18 -0800

    revised update IsThisThingOn.java
    added in capital letters

--- homework0129/IsThisThingOn.java
+++ homework0129/IsThisThingOn.java
@@ -1,18 +1,18 @@
 public class IsThisThingOn {
 	public static void main(String[] args) {
-		if(args.length == 0) {
+		if (args.length == 0) {
 			System.out.println("no. please enter a string");
 		}
-		for (int i=0; i< args.length; i++){
+		for (int i=0; i< args.length; i++) {
 			System.out.println(countTheVowels(args[i]));
 		}
 	}
 	
-	public static int countTheVowels(String text){
+	public static int countTheVowels(String text) {
 		int count = 0;
-		for(int index = 0; index < text.length(); index++){
+		for (int index = 0; index < text.length(); index++) {
 			char letter = text.charAt(index);
-			if(letter == 'a'|| letter=='e'|| letter=='i'||letter=='o'||letter=='u') {
+			if (letter == 'a'|| letter == 'A' || letter == 'e' || letter == 'E' || letter == 'i' || letter == 'I' || letter == 'o' || letter == 'O' || letter == 'u' || letter == 'U') {
 				count += 1;
 			}
 		}
@@ -20,5 +20,3 @@ public class IsThisThingOn {
 	
 	}
 }
-
-
