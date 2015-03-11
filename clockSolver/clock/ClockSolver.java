c5c69407bbfb51d0d56e47f08beb15802c085179
Megan Karbowski <mkarbows@lion.lmu.edu>
2015-03-10 18:50:59 -0700

    everything but main commit ClockSolver.java
    completed almost everything but the main method

--- clockSolver/clock/ClockSolver.java
+++ clockSolver/clock/ClockSolver.java
@@ -8,6 +8,10 @@ package clock;
 public class ClockSolver {
 
     // TODO: Add instance variables/constants here.
+    private Clock clock;
+    private double angleBetween;
+    private double goalAngle;
+    private double currentAngle;
 
     /**
      * Constructs a ClockSolver Object with the given angle to find and
@@ -15,6 +19,36 @@ public class ClockSolver {
      */
     public ClockSolver(double angleToFind) {
         // TODO: Finish this constructor.
+        this(angleToFind, 1.0);
+
+        
+    }
+    public ClockSolver(double angleToFind, double grain) {
+        clock = new Clock(12, 0, 0, grain);
+        goalAngle = angleToFind;
+        angleBetween = 0;
+        if (angleToFind < 0 || grain <= 0.0) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public String getClockTime() {
+        return this.clock.toString();
+    }
+    public double getSecondsPassed() {
+        return this.clock.getSecondsPassed();
+    }
+    public double getAngleBetween() {
+        return angleBetween; 
+    }
+    public void tickClock() {
+        clock.tick();
+        angleBetween = (angleBetween + (this.clock.getGrain()*(5.5/60))) % 360;
+    }
+    public boolean foundAngle() {
+        return (angleBetween >= this.goalAngle - ((5.5/60)*this.clock.getGrain()) && 
+            angleBetween < this.goalAngle + ((5.5/60)*this.clock.getGrain())) ||
+            (360 - angleBetween >= this.goalAngle - ((5.5/60)*this.clock.getGrain()) &&
+            360 - angleBetween < this.goalAngle - ((5.5/60)*this.clock.getGrain()));
     }
 
     // TODO: Add other constructor and methods.
