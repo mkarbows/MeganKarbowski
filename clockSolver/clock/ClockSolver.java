9473d6d494c62df1f3809f302746db4d6995c4d8
Megan Karbowski <mkarbows@lion.lmu.edu>
2015-03-11 23:52:29 -0700

    almost done commit ClockSolver.java
    have to finish the last part of the main method

--- clockSolver/clock/ClockSolver.java
+++ clockSolver/clock/ClockSolver.java
@@ -19,15 +19,13 @@ public class ClockSolver {
      */
     public ClockSolver(double angleToFind) {
         // TODO: Finish this constructor.
-        this(angleToFind, 1.0);
-
-        
+        this(angleToFind, 1.0);        
     }
     public ClockSolver(double angleToFind, double grain) {
         clock = new Clock(12, 0, 0, grain);
         goalAngle = angleToFind;
         angleBetween = 0;
-        if (angleToFind < 0 || grain <= 0.0) {
+        if (angleToFind < 0 || grain <= 0.0 || grain > 1800) {
             throw new IllegalArgumentException();
         }
     }
@@ -45,16 +43,50 @@ public class ClockSolver {
         angleBetween = (angleBetween + (this.clock.getGrain()*(5.5/60))) % 360;
     }
     public boolean foundAngle() {
-        return (angleBetween >= this.goalAngle - ((5.5/60)*this.clock.getGrain()) && 
-            angleBetween < this.goalAngle + ((5.5/60)*this.clock.getGrain())) ||
-            (360 - angleBetween >= this.goalAngle - ((5.5/60)*this.clock.getGrain()) &&
-            360 - angleBetween < this.goalAngle - ((5.5/60)*this.clock.getGrain()));
+        return (angleBetween >= this.goalAngle - ((5.5/60)*this.clock.getGrain()/2) && 
+            angleBetween < this.goalAngle + ((5.5/60)*this.clock.getGrain()/2)) ||
+            (360 - angleBetween >= this.goalAngle - ((5.5/60)*this.clock.getGrain()/2) &&
+            360 - angleBetween < this.goalAngle + ((5.5/60)*this.clock.getGrain()/2));
     }
-
     // TODO: Add other constructor and methods.
 
     public static void main(String[] args) {
         // TODO: Print some stuff here.
-    }
+        if (args.length == 0 || args.length > 2) {
+            System.out.println("Usage: java clock.ClockSolver <angleToFind> <grain(optional)>");
+            return;
+        }
+        double angleToFind = -1;
+        double grain = 1.0;
+        try {
+            angleToFind = Double.parseDouble(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Given angle is not a valid number");
+            return;
+        }
+        if (angleToFind < 0) {
+            System.out.println("Angle must be >= 0");
+            return;
+        }
+        if (angleToFind > 360) {
+            angleToFind %= 360;
+        }
 
+        try { 
+            if (args.length == 2) {
+               grain = Double.parseDouble(args[1]); 
+            } 
+        } catch (NumberFormatException e) {
+                System.out.println("Grain not a number. Using default: 1.0s");
+        }
+        if (grain <= 0) {
+            System.out.println("Grain must be > 0.0. Using default: 1.0s");
+        }
+        if (grain > 1800.0) {
+            System.out.println("Grain must be <= 1800.0s. Using default: 1.0s");
+        }
+
+        ClockSolver finalClock = new ClockSolver(angleToFind, grain);
+        //for (int i = 0; i < )
+    }
 }
