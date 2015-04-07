5cdeb48d1ba65f2a1865ca4459e47faacaefe55e
Megan Karbowski <mkarbows@lion.lmu.edu>
2015-04-07 12:57:40 -0700

    second commit Fibonacci.java
    added a try catch

--- bFInt/math/Fibonacci.java
+++ bFInt/math/Fibonacci.java
@@ -2,27 +2,32 @@ package math;
 
 public class Fibonacci {
 	public static void main(String[] args) {
-		if (args.length < 1) {
-			System.out.println("...");
-			return;
-		}
+		try {
+			if (args.length < 1) {
+				System.out.println("Please enter a number");
+				return;
+			}
 		
-		BFInt number = new BFInt(args[0]);
-		if (number.equals(BFInt.ONE)) {
-			System.out.println("0");
-			return;
-		} else if (number.equals(new BFInt(2))) {
-			System.out.println("1");
-			return;
-		}
-		BFInt firstSeed = new BFInt("0");
-		BFInt secondSeed = new BFInt("1");
+			BFInt number = new BFInt(args[0]);
+			if (number.equals(BFInt.ONE)) {
+				System.out.println("0");
+				return;
+			} else if (number.equals(new BFInt(2))) {
+				System.out.println("1");
+				return;
+			}
+			BFInt firstSeed = new BFInt("0");
+			BFInt secondSeed = new BFInt("1");
 
-		for (BFInt i = new BFInt(2); i.isLessThan(number); i = i.plus(new BFInt(1))) {
-			BFInt sum = firstSeed.plus(secondSeed);
-			firstSeed = new BFInt(secondSeed.toString());
-			secondSeed = new BFInt(sum.toString());
+			for (BFInt i = new BFInt(2); i.isLessThan(number); i = i.plus(new BFInt(1))) {
+				BFInt sum = firstSeed.plus(secondSeed);
+				firstSeed = new BFInt(secondSeed.toString());
+				secondSeed = new BFInt(sum.toString());
+			}
+			System.out.println(secondSeed);
+		} catch (IllegalArgumentException e) {
+			System.out.println("Please enter a number");
+			return;
 		}
-		System.out.println(secondSeed);
 	}
 }
