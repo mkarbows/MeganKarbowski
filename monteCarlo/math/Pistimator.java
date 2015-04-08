163dec040c523cad1c80a3a10e86b3e50a4a4124
Megan Karbowski <mkarbows@lion.lmu.edu>
2015-04-07 17:15:55 -0700

    initial commit of Pistimator.java
    need to write an if statement for if the user enters a negative number

--- monteCarlo/math/Pistimator.java
+++ monteCarlo/math/Pistimator.java
@@ -9,6 +9,54 @@ package math;
 public class Pistimator {
 
     // TODO: Add instance variables, constructors and methods.
+    private long darts;
+    private long hits;
+
+    public Pistimator() {
+    	this(100000);
+    }
+
+    public Pistimator(long darts) {
+    	hits = 0;
+    	this.darts = darts;
+    }
+
+    public double estimatePi() {
+    	System.out.println("start");
+    	for (int i = 0; i < darts; i++) {
+			double x = 2 * Math.random() - 1;
+			double y = 2 * Math.random() - 1;
+			printCoordinates(x, y, x*x + y*y <= 1); 
+			if (x*x + y*y <= 1) {
+				hits++; 
+			} 
+		}
+		System.out.println("end");	 
+		return 4 * ((double) hits / (double) darts);
+    }
+
+    public long getHits() {
+    	return hits;
+    }
+
+    public static void main(String[] args) {
+	    try {
+	    	
+	    	Pistimator newPi;
+	    	double piEstimate;
+	    	if (args.length == 0) {
+	    		newPi = new Pistimator();
+	    	} else {
+	    		newPi = new Pistimator(Long.parseLong(args[0]));
+	    	}
+	    	piEstimate = newPi.estimatePi();
+	    	System.out.println("Number of hits: " + newPi.getHits());
+	    	System.out.println("Number of darts: " + newPi.darts);
+	    	System.out.println("Pi Estimate: " + piEstimate);
+	    } catch (Exception e) {
+	    	System.out.println("This isn't a long/int");
+	    }
+    }
 
     private static void printCoordinates(double x, double y, boolean in) {
         System.out.println(x + " " + y + " " + (in ? "in" : "out"));
