56118ae38a58f19b90c5039866ed36e7a2cda98b
Megan Karbowski <mkarbows@lion.lmu.edu>
2015-04-09 17:06:53 -0700

    initial commit Polynomial.java
    finished the Polynomial.java

--- monteCarlo/math/Polynomial.java
+++ monteCarlo/math/Polynomial.java
@@ -3,9 +3,26 @@ package math;
 public class Polynomial implements Integratable {
 
     // TODO: Add instance variables, constructors and methods.
+	private double[] coefficients;
 
-    @Override
-    public double f(double x) {
-        return 0.0;
+    public Polynomial() {
+    	this.coefficients = new double[] {1.0};
     }
+
+    public Polynomial(double[] coefficients) {
+    	this.coefficients = coefficients;
+    }
+
+    public double f(double x) {
+    	int power = coefficients.length - 1;
+    	double result = 0;
+    	for (int i = 0; i < coefficients.length; i++) {
+    		result += coefficients[i] * Math.pow(x, power);
+    		power --;
+    	}
+    	return result;
+	}
 }
+
+
+
