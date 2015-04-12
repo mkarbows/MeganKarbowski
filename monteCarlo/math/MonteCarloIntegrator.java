a0ca189a84a873ecdc640bd71e4753bd48f92853
Megan Karbowski <mkarbows@lion.lmu.edu>
2015-04-11 17:29:11 -0700

    initial commit MonteCarloIntegrator.java
    need to finish the main method and catch exceptions and more

--- monteCarlo/math/MonteCarloIntegrator.java
+++ monteCarlo/math/MonteCarloIntegrator.java
@@ -3,10 +3,63 @@ package math;
 public class MonteCarloIntegrator {
 
     // TODO: Add instance variables, constructors and methods.
+	private Integratable function;
+	private long darts = 1000000; 
+	private long hits;
 
+    public MonteCarloIntegrator(Integratable function) {
+    	this.function = function;
+    }
+
+    public double integrate(double lowerBound, double upperBound) {
+    	double rangeX = upperBound - lowerBound;
+    	double ceiling = 0;
+    	double floor = 0;
+    	double rangeY = ceiling - floor;
+    	for (int i = 0; i < 100; i++) {
+    		double y = function.f(lowerBound + (i/100) * rangeX);
+    		if (y > ceiling) {
+    			ceiling = y;
+    		} else if (y < floor) {
+    			floor = y;
+    		}
+    	}
+    	for (int i = 0; i < darts; i++) {
+			double randomX = Math.random() * rangeX + lowerBound;
+			double randomY = Math.random() * rangeY + floor;
+			double actualY = function.f(randomX);
+			printCoordinates(randomX, randomY, randomY <= actualY);
+			if (randomY <= actualY) {
+				hits++;
+			}
+		}
+		return hits;
+    }
+
+    public double integrate(double lowerBound, double upperBound, long darts) {
+    	this.darts = darts;
+    	return integrate(lowerBound, upperBound);
+    }
+
+    public static void main(String[] args) {
+    	if (args.length < 4) {
+    		System.out.println("Please enter at least 3 numbers");
+    		return;
+    	} 
+    	long darts = Long.parseLong(args[0]);
+    	double lowerBound = Double.parseDouble(args[1]);
+    	double upperBound = Double.parseDouble(args[2]);
+    	int numOfCoeff = args.length - 3;
+    	double[] coeff = new double[numOfCoeff];
+    	for (int i = 3; i < args.length; i++) {
+    		coeff[i - 3] = Double.parseDouble(args[i]);
+    	}
+    	// Polynomial p = new Polynomial(coeff);
+    	// MonteCarloIntegrator myIntegrator = MonteCarloIntegrator(p);
+    	// System.out.println("Number of darts: " + p.darts);
+    }
 
     private static void printCoordinates(double x, double y, boolean in) {
         System.out.println(x + " " + y + " " + (in ? "in" : "out"));
     }
-
 }
